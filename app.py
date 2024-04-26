dbec6823fe203f3bd8fcd5530bb6abc934eee1bd
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-04-26 13:44:57 +0900

    ルーティング設定

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return '<h1>HELLO WORLD</h1>'
 
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return f"{post_id}"
+     
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port='5001' )
