77195ed6772ecedbe24cc7d2f124663cb43d6312
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-08 21:44:21 +0900

    ログアウト

--- flaskr/views.py
+++ flaskr/views.py
@@ -13,3 +13,12 @@ from flaskr import db
 from os import path
 
 bp = Blueprint('app', __name__, url_prefix='')
+
+@bp.route('/')
+def home():
+    return render_template('home.html')
+
+@bp.route('/logout')
+def logout_user():
+    logout_user()
+    return redirect(url_for('app.home'))
