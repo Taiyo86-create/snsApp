160e280f8de38c38100e6f15c8487afe87097c73
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-08 14:59:51 +0900

    Update models.py

--- flaskr/models.py
+++ flaskr/models.py
@@ -5,3 +5,7 @@ from flask_login import UserMixin
 
 from datetime import datetime, timedelta
 from uuid import uuid4
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
