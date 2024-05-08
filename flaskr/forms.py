7068779e6062e56bfa818197ba48328590807c7c
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-05-08 22:05:43 +0900

    ログイン用フォーム

--- flaskr/forms.py
+++ flaskr/forms.py
@@ -4,3 +4,12 @@ from wtforms.fields import(
 )
 from wtforms.validators import DataRequired, Email, EqualTo
 from wtforms import ValidationError
+
+
+# ログイン用フォーム
+class LoginForm(Form):
+    email = StringField('メールアドレス: ', validators=[DataRequired(), Email()])
+    password = PasswordField('パスワード:', validators=[DataRequired(), EqualTo('confirm_password', message='パスワードが一致しません')])
+    confirm_password = PasswordField('パスワード再入力: ', validators=[DataRequired()])
+    submit = SubmitField('ログイン')
+#登録用フォーム
